91a4c603f6bf0e18c8efdea3e3edcfa40ccc3eca
sabareshvsb <sabareshvsb1@gmail.com>
2026-08-05 21:10:24 +0530

    Crop USDX logo into a round icon for favicon and hero

--- src/components/LandingExperience.jsx
+++ src/components/LandingExperience.jsx
@@ -409,7 +409,7 @@ export default function LandingExperience() {
           <div className="orbital-ring ring-one" /><div className="orbital-ring ring-two" />
           <div className="core-orb">
             <Image
-              src="/images/usdx-logo.png"
+              src="/images/usdxlogo-round.png"
               alt="USDX Smart Contract coin"
               width={340}
               height={340}
